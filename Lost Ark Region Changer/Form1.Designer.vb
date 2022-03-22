65413560a788c828040859365c612ddf1c8123e3
Andy <Andy@NB-ANDY01>
2022-03-22 14:28:19 +0100

    Projektdateien hinzufügen.

---
+++ Lost Ark Region Changer/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cbRegion = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cbRegion
+        '
+        Me.cbRegion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbRegion.FormattingEnabled = True
+        Me.cbRegion.Location = New System.Drawing.Point(12, 12)
+        Me.cbRegion.Name = "cbRegion"
+        Me.cbRegion.Size = New System.Drawing.Size(312, 33)
+        Me.cbRegion.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(353, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(196, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start Lost Ark"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(575, 65)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cbRegion)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MdiChildrenMinimizedAnchorBottom = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Lost Ark Region Changer"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cbRegion As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Lost Ark Region Changer/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Xml
+Public Class Form1
+    Dim strSteamInstallPath As String = Nothing
+    Dim strRegionCode As String = "CE"
+    Dim strConfig As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Environment.Is64BitOperatingSystem Then
+
+            strSteamInstallPath = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam", "InstallPath", Nothing)
+        Else
+            strSteamInstallPath = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", Nothing)
+        End If
+        If IsNothing(strSteamInstallPath) Then
+            MsgBox("No Steam-Client-Installation found! Exiting")
+            Application.Exit()
+        End If
+        strConfig = strSteamInstallPath & "\steamapps\common\Lost Ark\EFGame\Config\UserOption.xml"
+
+        Dim xml As New XmlDocument
+        xml.Load(strConfig)
+        If IsNothing(xml.InnerXml) Then
+            MsgBox("Lost Ark Confing not found! Exiting")
+            Application.Exit()
+        End If
+        Dim regionNode As XmlNode = xml.SelectSingleNode("UserOption/SaveAccountOptionData/RegionID")
+        strRegionCode = regionNode.InnerText
+
+        cbRegion.Items.Clear()
+        cbRegion.Items.Add("Europe Central")
+        cbRegion.Items.Add("Europe West")
+        cbRegion.Items.Add("North America East")
+        cbRegion.Items.Add("North America West")
+        cbRegion.Items.Add("Southamerica")
+        cbRegion.SelectedItem = getSelectedFromCode(strRegionCode)
+
+    End Sub
+
+    Private Function getSelectedFromCode(ByVal strCode As String)
+        If strCode = "EA" Then
+            Return "North America East"
+        ElseIf strCode = "WA" Then
+            Return "North America West"
+        ElseIf strCode = "SA" Then
+            Return "Southamerica"
+        ElseIf strCode = "WE" Then
+            Return "Europe West"
+        Else
+            Return "Europe Central"
+        End If
+    End Function
+
+    Private Function getCodeFromSelected(ByVal strCode As String)
+        If strCode = "North America East" Then
+            Return "EA"
+        ElseIf strCode = "North America West" Then
+            Return "WA"
+        ElseIf strCode = "Southamerica" Then
+            Return "SA"
+        ElseIf strCode = "Europe West" Then
+            Return "WE"
+        Else
+            Return "CE"
+        End If
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim xml As New XmlDocument
+        xml.Load(strConfig)
+        xml.SelectSingleNode("UserOption/SaveAccountOptionData/RegionID").InnerText = strRegionCode
+        Dim xw As XmlWriter = XmlWriter.Create(strConfig)
+        xml.WriteTo(xw)
+        xw.Close()
+        'Process.Start("steam://rungameid/1599340")
+        Dim psi As ProcessStartInfo = New ProcessStartInfo("steam://rungameid/1599340")
+        psi.UseShellExecute = True
+        Process.Start(psi)
+        Application.Exit()
+    End Sub
+
+    Private Sub cbRegion_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbRegion.SelectedIndexChanged
+        strRegionCode = getCodeFromSelected(cbRegion.SelectedItem)
+
+    End Sub
+End Class
